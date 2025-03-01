48eb7282babac34a9e45ca1bf06966842bfdc800
Mariana Cervantes <marianac080923>
2025-02-28 23:36:36 -0500

    VS123-30 Crear formulario con su respectivo campo (Documento) y boton para impresar usando componentes de bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginChildrenComponent } from './login-children/login-children.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginChildrenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/login-children/login-children.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-children',
+  imports: [],
+  templateUrl: './login-children.component.html',
+  styleUrl: './login-children.component.css'
+})
+export class LoginChildrenComponent {
+
+}

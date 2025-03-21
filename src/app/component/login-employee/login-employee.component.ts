2857b135fc12e2c10915ed15d5207ae84fd4cb4b
Nelson Zaraza <nelsonmauricionavarrozaraza@gmail.com>
2025-03-20 21:52:48 -0500

    VS123-40 feat(auth): Connect employee login API with frontend

--- src/app/component/login-employee/login-employee.component.ts
+++ src/app/component/login-employee/login-employee.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Credenciales } from './credencialesEmployee';
+import { LoginEmployeeService } from '../../login-employee.service';
 
 @Component({
   selector: 'app-login-employee',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login-employee.component.html',
-  styleUrl: './login-employee.component.css'
+  styleUrl: './login-employee.component.css',
 })
 export class LoginEmployeeComponent {
+  credenciales: Credenciales = new Credenciales();
 
+  constructor(private loginEmployeeService: LoginEmployeeService) {}
+
+  onSubmit() {
+    console.log("Se hizo clic en el botón de login");
+    this.logeearse();
+  }
+  
+
+  logeearse() {
+    console.log('Botón presionado');
+    this.loginEmployeeService.login(this.credenciales).subscribe({
+      next: (datos) => {
+        console.log('Respuesta completa del backend:', datos);
+      },
+      error: (error: any) => {
+        console.log('Error en la petición:', error);
+      },
+    });
+  }  
 }

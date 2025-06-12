e0879073cda761385e2bafe5f50a8ab7f7e2f756
Mariana cervantes <mariana080923@gmail.com>
2025-06-12 10:57:15 -0500

    VS123-73 Creacion del componente y la tabla para realizar el historial de acceso de usuarios

---
+++ src/app/access-history/access-history.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-access-history',
+  imports: [],
+  templateUrl: './access-history.component.html',
+  styleUrl: './access-history.component.css'
+})
+export class AccessHistoryComponent {
+
+}
